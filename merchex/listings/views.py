a93014453252e34f55142570e8e726c84cca0e9a
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-30 15:39:11 +0200

    more views and urls

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listing</h1> <p>Nous adorons merch !</p>')
+
+def contact_us(request):
+    return HttpResponse('<h1>Nous contacter</h1> <p>Nous adorons merch !</p>')
